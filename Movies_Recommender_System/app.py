9a51100837da8675c7fd9b6ecdcb3d37e33f810e
arkam29 <115999800+Arkam102@users.noreply.github.com>
2024-08-24 12:07:23 +0530

    Update app.py

--- Movies_Recommender_System/app.py
+++ Movies_Recommender_System/app.py
@@ -1,13 +1,19 @@
 import pickle
 import streamlit as st
 import requests
+import gdown
 from jinja2 import Template
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-api_key = os.getenv('TMDB_API_KEY')
+api_key = st.secrets["TMDB_API_KEY"]
+
+# Google Drive File ID for similarity.pkl
+file_id = '1YZ7ElUf43aptn0kH87mwGSEbZ5MS1f4I'
+output = 'similarity.pkl'
+
 # Function to fetch movie details
+def download_similarity_file():
+    url = f'https://drive.google.com/uc?id={file_id}'
+    gdown.download(url, output, quiet=False)
 def fetch_movie_details(id):
     url = f"https://api.themoviedb.org/3/movie/{id}?api_key={api_key}"
     data = requests.get(url).json()
@@ -45,10 +51,13 @@ def render_template(context):
         template = Template(f.read())
     return template.render(context)
 
+if not os.path.exists(output):
+    download_similarity_file()
+
 # Main code
 st.markdown('<div class="header">Movie Recommender System</div>', unsafe_allow_html=True)
 movies = pickle.load(open('D:\Projects\Movie_Recommender\movie_rec_system\movies.pkl', 'rb'))
-similarity = pickle.load(open('D:\Projects\Movie_Recommender\movie_rec_system\similarity.pkl', 'rb'))
+similarity = pickle.load(open(output, 'rb'))
 
 load_css()
 
